6f608caca66fea50c1bddb0fe3c092cafad6f2e2
FrostyMemes <lager11197@yandex.ru>
2022-03-14 00:15:46 +0300

    Created interfaces for realization interaction with database

---
+++ FirstWeb/Domain/Entities/Repositories/Abstract/IServiceItemsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mime;
+
+namespace FirstWeb.Domain.Entities.Repositories.Abstract
+{
+    public interface IServiceItemsRepository
+    {
+        IQueryable<ServiceItem> GetServiceItems();
+        ServiceItem GetServiceItemById(Guid id);
+        void SaveServiceItem(ServiceItem entity);
+        void DeleteServiceItem(Guid id);
+    }
+}
---
+++ FirstWeb/Domain/Entities/Repositories/Abstract/ITextItemsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mime;
+
+namespace FirstWeb.Domain.Entities.Repositories.Abstract
+{
+    public interface ITextItemsRepository
+    {
+        IQueryable<TextField> GetTextFields();
+        TextField GetTextFieldById(Guid id);
+        TextField GetTextFieldByCodeWord(string codeWord);
+        void SaveTextField(TextField entity);
+        void DeleteTextField(Guid id);
+    }
+}
