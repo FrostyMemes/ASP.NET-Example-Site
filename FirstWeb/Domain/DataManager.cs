790d2116d06099d26e0d21993ab8951f21f94808
FrostyMemes <lager11197@yandex.ru>
2022-03-14 21:23:35 +0300

    Create data manager for DataBase

---
+++ FirstWeb/Domain/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using FirstWeb.Domain.Repositories.Abstract;
+
+namespace FirstWeb.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepository TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepository textFieldsRepository, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepository;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
