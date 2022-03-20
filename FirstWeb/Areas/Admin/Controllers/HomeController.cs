890a8f366336103cae6f315c4a2d46d442e21fa3
FrostyMemes <lager11197@yandex.ru>
2022-03-20 21:12:41 +0300

    Realized area for admin

---
+++ FirstWeb/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWeb.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- FirstWeb/Service/AdminAreaAutorization.cs
+++ FirstWeb/Service/AdminAreaAutorization.cs
@@ -20,11 +20,9 @@ namespace FirstWeb.Service
         public void Apply(ControllerModel controller)
         {
             if (controller.Attributes.Any(a =>
-                    a is AreaAttribute &&
-                    (a as AreaAttribute).RouteValue.Equals(area, StringComparison.OrdinalIgnoreCase))
+                    a is AreaAttribute && (a as AreaAttribute).RouteValue.Equals(area, StringComparison.OrdinalIgnoreCase))
                 || controller.RouteValues.Any(r =>
-                    r.Key.Equals("area", StringComparison.OrdinalIgnoreCase) &&
-                    r.Value.Equals(area, StringComparison.OrdinalIgnoreCase)))
+                    r.Key.Equals("area", StringComparison.OrdinalIgnoreCase) && r.Value.Equals(area, StringComparison.OrdinalIgnoreCase)))
             {
                 controller.Filters.Add(new AuthorizeFilter(policy));
             }
