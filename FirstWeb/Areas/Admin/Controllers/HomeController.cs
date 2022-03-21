1069406a5c5d3f932e043b91954240ac6f38ec12
FrostyMemes <lager11197@yandex.ru>
2022-03-22 00:18:15 +0300

    Preparation for admin area controller

--- FirstWeb/Areas/Admin/Controllers/HomeController.cs
+++ FirstWeb/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstWeb.Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.ServiceItems.GetServiceItems());
         }
     }
 }
