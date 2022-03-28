f779ad346b96bbfa927cec8def59e7d053e080df
FrostyMemes <lager11197@yandex.ru>
2022-03-28 23:32:39 +0300

    Add Contacts method for connecting to contacts view

--- services/Controllers/HomeController.cs
+++ services/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using services.Domain;
 
 namespace services.Controllers
 {
     public class HomeController : Controller
     {
-        // GET
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+        
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
